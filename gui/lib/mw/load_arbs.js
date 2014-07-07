16017dfc4d39daa69d6b1987c965a692a9c12eff
Myles Recny <mkrecny@gmail.com>
2014-07-07 11:34:32 -0400

    margins and no logging of arbs

--- gui/lib/mw/load_arbs.js
+++ gui/lib/mw/load_arbs.js
@@ -1,7 +1,6 @@
 
 module.exports = function(req,res,next){
 	DAO.getMarketArbs(req.params.market, function(err, arbs){
-		console.log(arbs);
 		res.locals.arbs= {market:req.params.market, arbs:arbs};
 		next();
 	});
