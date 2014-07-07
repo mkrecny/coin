0c8a3e376585d8f560c617b537e08687ecd66f71
Myles Recny <mkrecny@gmail.com>
2014-07-07 12:51:30 -0400

    stuff

--- depth.js
+++ depth.js
@@ -1,12 +1,11 @@
 var Marketstem = require('./lib/marketstem.js')
 , DAO = require('./lib/dao.js')
 
-
 var marketstem = new Marketstem();
 
 marketstem.on('data', function(data){
 	DAO.setMarketDepth(data.market, data.exchange, data.asks, data.bids, function(err, res){
-		console.log(new Date(), 'depth updated');
+		console.log(new Date(), 'depth updated @', process.uptime());
 	});
 });
 
